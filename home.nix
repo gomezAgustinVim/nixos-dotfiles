4435f210a65e3de24c4400a881c8f2a3ceeaa097
lordagustin22 <niconecrofilo589@gmail.com>
2026-06-20 02:20:21 -0300

    added a bajillion configs

--- home.nix
+++ home.nix
@@ -74,5 +74,6 @@
   home.file.".config/hypr".source = ./config/hypr;
   home.file.".config/nvim".source = ./config/nvim;
   home.file.".config/waybar".source = ./config/waybar;
+  home.file.".config/tmux".source = ./config/tmux;
 
 }
